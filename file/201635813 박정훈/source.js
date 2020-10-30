bac848e83a2a3a2760723063df48b38c48d1b084
park-jun-hun <63997284+park-jun-hun@users.noreply.github.com>
2020-10-30 23:07:20 +0900

    commit with lee

--- file/201635813 박정훈/source.js
+++ file/201635813 박정훈/source.js
@@ -11,6 +11,7 @@ var w_point=[0,0,0,0,0,0];
 var direction = 1;
 var theta=0;
 var thetaLoc;
+var start=true;
 
 window.onload = function init()
 {
@@ -33,7 +34,7 @@ window.onload = function init()
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     // background
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
     
@@ -154,13 +155,43 @@ window.onload = function init()
         vec2(-2.0,7.5),
         vec2(-2.5,8.0),
     ]
-  
+    var back = [
+        vec2(-0.8, 0.9),
+        vec2(-0.85, 1.0),
+        vec2(-0.95, 1.0),
+        vec2(-1.0, 0.9),
+        vec2(-0.95, 0.8),
+        vec2(-0.85, 0.8),
+        vec2(-0.8, 0.9)
+    ];
 
 
-
-
+// click pause 
+document.getElementById("Pause").onclick=function(event){
+    start=false;
+}
+// click restart
+document.getElementById("Restart").onclick=function(event){
+    start=true;
+    object();
+}
+// select level 
+document.getElementById("Difficulty").onclick=function(event){
+    switch(event.target.index){
+        case 0:
+            alert("level1");
+            break;
+        case 1:
+            alert("level2");
+            break;
+        case 2:
+            alert("level3");
+            break;
+    }
+}
 // using mouse click down
     canvas.addEventListener("mousemove",function(event){
+        if(start){
         var p= vec2(2*event.clientX/canvas.width -1,
             2*(canvas.height-event.clientY)/canvas.height-1);
             var distance = Math.sqrt(p[0]*p[0] +p[1]*p[1]);
@@ -183,10 +214,16 @@ window.onload = function init()
     // theta -=1.57
       gl.uniform1f(thetaLoc, theta);
       object();
+        }
     });
+
     object();
     function object(){
         var count=0;
+        var row=0;
+        
+    
+
         draw(gl.TRIANGLES, cusor, cusor_color,[0.0,0.0,0.0,8.5]);
         draw(gl.LINE_STRIP, cusor_line, cusor_line_color,[0.0,0.0,0.0,8.5]);
         draw(gl.TRIANGLE_FAN,head,head_color,[0.0,0.0,0.0,10.0]);
@@ -196,10 +233,24 @@ window.onload = function init()
         for(count=0;count<10;count++){
         draw(gl.TRIANGLE_FAN,head,head_color,[0.0,-count,0.0,10+count]);
         }
-        
+
+        // 고정값 
+        gl.uniform1f(thetaLoc,0);
+        // back
+        for(row=0;row>-3;row-=0.2){
+            for(count=0;count<2;count+=0.2){
+            draw(gl.LINE_LOOP, back, cusor_color,[count,row,0.1,0.0]);
+            }
+    }
+    draw(gl.TRIANGLE_FAN,left_eye,cusor_line_color,[2.5,-6.0,1.0,0.1]);
+    draw(gl.TRIANGLE_FAN,left_eye,cusor_line_color,[3.5,-6.0,1.0,0.1]);
+    draw(gl.TRIANGLE_FAN,left_eye,cusor_line_color,[2.5,-7.5,1.0,0.1]);
+    draw(gl.TRIANGLE_FAN,left_eye,cusor_line_color,[3.5,-7.5,1.0,0.1]);
+    // draw(gl.TRIANGLE_FAN,left_eye,cusor_line_color,[4.0,-6.0,1.0,0.5]);
     }
     // drawing square
     function draw(shape,vertex,color,offset){
+       
         var buffer = gl.createBuffer();
         gl.bindBuffer( gl.ARRAY_BUFFER, buffer);
         gl.bufferData( gl.ARRAY_BUFFER,flatten(vertex), gl.STATIC_DRAW );
