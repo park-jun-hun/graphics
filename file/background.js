bb4b0551987d78cdf2cf718fbfb37fa605433a51
이정명 <57949168+leejungmyung97@users.noreply.github.com>
2020-10-31 01:46:48 +0900

    Update background.js

--- file/background.js
+++ file/background.js
@@ -1,78 +1,317 @@
 var gl;
 var points;
+var index=0;
+var state=false;
+var place=[];
+var delay=10;
+var intervalId;
+var SSVmove=0;
+var state=0;
+var w_point=[0,0,0,0,0,0];
+var direction = 1;
+var theta=0;
+var thetaLoc;
+var start=true;
 
 window.onload = function init()
 {
+
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 
-    //var vertices = new Float32Array([vec2(-1, -1), vec2(0, 1), vec2(1, -1)]);
-    var vertices = [
-        vec2(-0.8, 0.9),
-        vec2(-0.85, 1.0),
-        vec2(-0.95, 1.0),
-        vec2(-1.0, 0.9),
-        vec2(-0.95, 0.8),
-        vec2(-0.85, 0.8),
-        vec2(-0.8, 0.9)
-    ];
-    //  Configure WebGL
-
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    // Load the data into the GPU
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+    var vColor = gl.getAttribLocation(program, "vColor");
+    var vertexColorBufferId = gl.createBuffer();
+    var vOffset = gl.getUniformLocation(program,"vOffset");
 
-    // Associate vertex data buffer with shader variables
 
-    var vPosition = gl.getAttribLocation(program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    // background
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
 
-    var vOffset = gl.getUniformLocation(program, "vOffset");
-    gl.uniform4fv(vOffset, [0,0,0,0])
+    thetaLoc = gl.getUniformLocation(program, "theta");
 
-    var vColor = gl.getUniformLocation(program, "vColor");
-    gl.uniform4fv(vColor, [0,0,1,0.4])
 
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.LINE_LOOP, 0, 6 );
+    //square
+    var cursor = [
 
-    for ( x = 0.25; x < 2.0; x = x + 0.25) {
-        for ( y = 0.0; y > -2.0; y = y - 0.25) {
-            var bufferId1 = gl.createBuffer();
-            gl.bindBuffer( gl.ARRAY_BUFFER, bufferId1 );
-            gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-            draw(x, y);
+        vec2(0.0,2.5),
+        vec2(-0.5,2.0),
+        vec2(0.5,2.0),
+
+        vec2(-0.25,2.0),
+        vec2(-0.25,1.5),
+        vec2(0.25,2.25),
+
+        vec2(0.25,2.25),
+        vec2(-0.25,1.5),
+        vec2(0.25,1.5)
+
+    ];
+    var cursor_color=[
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0),
+        vec4(0.24,0.7,0.53,1.0)
+    ]
+    var background_color=[
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5),
+        vec4(0.5,0.5,0.5,0.5)
+    ]
+    var cursor_line=[
+        vec2(0.0,2.5),
+        vec2(-0.5,2.0),
+        vec2(-0.25,2.0),
+        vec2(-0.25,1.5),
+        vec2(0.25,1.5),
+        vec2(0.25,2.0),
+        vec2(0.5,2.0),
+        vec2(0.0,2.5)
+    ]
+
+    var cursor_line_color=[
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0),
+        vec4(0.0,0.0,0.0,1.0)
+    ]
+
+    var head =[
+        vec2(0.0,0.0),
+
+        vec2(0.5,1.0),
+        vec2(-0.5,1.0),
+
+        vec2(-1.0,0.5),
+        vec2(-1.0,-0.5),
+
+        vec2(-0.5,-1),
+        vec2(0.5,-1),
+
+        vec2(1.0,-0.5),
+        vec2(1.0,0.5),
+        vec2(0.5,1.0),
+
+    ];
+
+    var head_color=[
+        vec4(0.2,0.4,0.5,1.0),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5),
+        vec4(0.2,0.4,0.5,0.5)
+    ]
+    var right_eye=[
+        vec2(3.0,7.0),
+
+        vec2(3.5,8.0),
+        vec2(2.5,8.0),
+
+        vec2(2.0,7.5),
+        vec2(2.0,6.5),
+
+        vec2(2.5,6),
+        vec2(3.5,6),
+
+        vec2(4.0,6.5),
+        vec2(4.0,7.5),
+        vec2(3.5,8.0),
+    ]
+    var left_eye=[
+        vec2(-3.0,7.0),
+
+        vec2(-2.5,8.0),
+        vec2(-3.5,8.0),
+
+        vec2(-4.0,7.5),
+        vec2(-4.0,6.5),
+
+        vec2(-3.5,6),
+        vec2(-2.5,6),
+
+        vec2(-2.0,6.5),
+        vec2(-2.0,7.5),
+        vec2(-2.5,8.0),
+    ]
+    var back = [
+        vec2(-0.8, 0.9),
+        vec2(-0.83, 0.96),
+        vec2(-0.89, 0.96),
+        vec2(-0.92, 0.9),
+        vec2(-0.89, 0.84),
+        vec2(-0.83, 0.84),
+        vec2(-0.8, 0.9)
+    ];
+
+
+// click pause
+    document.getElementById("Pause").onclick=function(event){
+        start=false;
     }
-}
+// click restart
+    document.getElementById("Restart").onclick=function(event){
+        start=true;
+        object();
+    }
+// select level
+    document.getElementById("Difficulty").onclick=function(event){
+        switch(event.target.index){
+            case 0:
+                alert("level1");
+                break;
+            case 1:
+                alert("level2");
+                break;
+            case 2:
+                alert("level3");
+                break;
+        }
+    }
+// using mouse click down
+    canvas.addEventListener("mousemove",function(event){
+        if(start){
+            var p= vec2(2*event.clientX/canvas.width -1,
+                2*(canvas.height-event.clientY)/canvas.height-1);
+            var distance = Math.sqrt(p[0]*p[0] +p[1]*p[1]);
+
+            if(p[0]>0&&p[1]>0){
+                theta=-1.57+Math.acos(p[0]/distance);
+            }
+            else if(p[0]<0&&p[1]>0){
+                theta=-1.57+Math.acos(p[0]/distance);
+            }
+            else if(p[0]<0&&p[1]<0){
+                theta=+1.57+Math.acos(-p[0]/distance);
+            }
+            else{
+                theta=+1.57+Math.acos(-p[0]/distance);
+            }
+            // 30도
+            // theta-=0.523;
+            // 90도
+            // theta -=1.57
+            gl.uniform1f(thetaLoc, theta);
+            object();
+        }
+    });
+
+    object();
+    function object(){
+        var count=0;
+        var row=0;
+
+
+
+        draw(gl.TRIANGLES, cursor, cursor_color,[0.0,0.0,0.0,8.5]);
+        draw(gl.LINE_STRIP, cursor_line, cursor_line_color,[0.0,0.0,0.0,8.5]);
+        draw(gl.TRIANGLE_FAN,head,head_color,[0.0,0.0,0.0,10.0]);
+        draw(gl.TRIANGLE_FAN,right_eye,cursor_line_color,[0.0,0.0,-1.0,100.0]);
+        draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[0.0,0.0,-1.0,100.0]);
+        // BODY
+        for(count=0;count<10;count++){
+            draw(gl.TRIANGLE_FAN,head,head_color,[0.0,-count,0.0,10+count]);
+        }
+
+        // 고정값
+        gl.uniform1f(thetaLoc,0);
+        // back
+        for(row=0.125;row>-3;row-=0.125){
+            for(count=-0.125;count<2;count+=0.125){
+                draw(gl.LINE_LOOP, back, cursor_color,[count,row,0.1,0.0]);
+            }
+        }
+        draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[2.5,-6.0,1.0,0.1]);
+        draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[3.5,-6.0,1.0,0.1]);
+        draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[2.5,-7.5,1.0,0.1]);
+        draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[3.5,-7.5,1.0,0.1]);
+        // draw(gl.TRIANGLE_FAN,left_eye,cursor_line_color,[4.0,-6.0,1.0,0.5]);
+    }
+    // drawing square
+    function draw(shape,vertex,color,offset){
+
+        var buffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, buffer);
+        gl.bufferData( gl.ARRAY_BUFFER,flatten(vertex), gl.STATIC_DRAW );
+
+        gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vPosition );
+
+        // object color
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBufferId);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(color), gl.STATIC_DRAW);
+
+        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vColor);
+
+        gl.uniform4fv(vOffset,offset);
+
+        // draw
+        render(shape,0,vertex.length);
+        // initialize postion
+        gl.uniform4fv(vOffset,[0,0,0,0]);
+    }
+
+
+
+
+    function MoveRender(){
+        for( i=0;i<6;i++){
+            w_point[i]+=(0.01*direction);
+        }
+
+        object();
+        requestAnimationFrame(MoveRender);
+
+
+    }
+
+
+
+    // if(state==1){
+    //     intervalId = setInterval(MoveRender,delay);
+    // }
+    // if(state==2){
+    //     intervalId = setInterval(MoveRender,delay);
+    // }
+
 };
 
-function draw(x, y){
-    // Associate vertex data buffer with shader variables
+// drawing each object
+function render(shape,start, end) {
 
-    var vPosition = gl.getAttribLocation(program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    var vOffset = gl.getUniformLocation(program, "vOffset");
-    gl.uniform4fv(vOffset, [x,y,0,0])
-
-    var vColor = gl.getUniformLocation(program, "vColor");
-    gl.uniform4fv(vColor, [0,0,1,0.4])
-
-    gl.drawArrays( gl.LINE_LOOP, 0, 6 );
+    gl.drawArrays( shape,start,end);
+    // window.requestAnimationFrame(render);
 }
-
